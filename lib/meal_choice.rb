747a40a2d671494e7eb6a048a5329a75bdb646ce
Elias Delgado <evdelgado99@gmail.com>
2020-08-27 06:23:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,18 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(veg1, veg2, protein = "tofu")
+  
+  puts "What a nutritious meal!"
+      
+  if meal_choice = ["carrots", "string beans", "lentils"]
+    return "A plate of #{protein} with #{veg1} and #{veg2}."
+    
+  elsif meal_choice = [] 
+    return "A plate of #{protein} with #{veg1} and #{veg2}."
+    
+  elsif meal_choice = [] 
+    return "A plate of #{protein} with #{veg1} and #{veg2}."
+  end
+  
+end
